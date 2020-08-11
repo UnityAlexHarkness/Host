2f2b1a27e6cb6aa796533d8f1ee5b4b0eb084692
Alex Harkness <alex.harkness@unity3d.com>
2020-08-11 10:58:59 -0700

    Initial commit

---
+++ Assets/GameController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject Game1Button;
+    public AssetReference Game1;
+
+    public GameObject Game2Button;
+    public AssetReference Game2;
+    
+
+    public void LoadGame1()
+    {
+        Game1Button.SetActive(false);
+        Game2Button.SetActive(false);
+
+        Addressables.LoadSceneAsync(Game1);
+    }
+
+    public void LoadGame2()
+    {
+        Game1Button.SetActive(false);
+        Game2Button.SetActive(false);
+
+        Addressables.LoadSceneAsync(Game2);
+    }
+}
